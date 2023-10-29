c63d69e4427e4f838cc1d0cf060506c0f4038a08
Pavel <zabolotnikovpg2@mail.ru>
2023-10-29 21:52:36 +0300

    instal_ts

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path =require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const HTMLInlineCSSWebpackPlugin = require('html-inline-css-webpack-plugin').default; 
+
+module.exports = {
+ entry: './src/index.ts',
+output: {
+path: path.resolve(__dirname, './dist'),
+filename: 'bundle.js',
+clean: true,
+},
+mode: 'production',
+plugins: [
+ new HtmlWebpackPlugin({
+    filname: './index.html',
+    template: './src/index.html',
+ }),
+ new HTMLInlineCSSWebpackPlugin(),
+ new MiniCssExtractPlugin({
+    filename: '[name].css',
+    chunkFilename: '[id].css'
+ })
+],
+module:{
+    rules:[
+        {
+            test: /\.css$/i,
+            use: [MiniCssExtractPlugin.loader, "css-loader"],
+          },
+          {
+            test: /\.tsx?$/,
+            use: 'ts-loader',
+            exclude: '/node_modules/',
+          },
+    ]
+}
+};
