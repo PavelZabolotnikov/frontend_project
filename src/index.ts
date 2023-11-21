b2f78246fcef7a234ba1563a22738a5a6c453603
Pavel <zabolotnikovpg2@mail.ru>
2023-11-21 23:00:19 +0300

    feat: homework done

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,34 @@
-import './index.css';
-const number = 100500;
-console.log(number); 
+// Задание 1
+function counterLogger() {
+  let count = 0;
+  function logCounter() {
+    console.log('count', count);
+  }
+
+  function incrementCounter() {
+    count++;
+  }
+  return {
+    logCounter,
+    incrementCounter,
+  };
+}
+const oneCounter = new counterLogger();
+oneCounter.incrementCounter();
+oneCounter.logCounter();
+
+const twoCounter = new counterLogger();
+twoCounter.incrementCounter();
+twoCounter.incrementCounter();
+twoCounter.logCounter();
+
+// Задание 2
+const user : { name: string, age: number, tasks: string[] } = {
+  age: 28,
+  name: 'Паша',
+  tasks:['arays-objects-methods', 'async_js', 'lint-format','scope_this'], 
+};
+
+user.tasks.push("typescript");
+console.log(user.tasks);
+
